da5d51a2f58539e7e35e0718de3bccd315ac3900
LuigiScorzelli <luigi.scorzelli87@gmail.com>
2018-08-10 14:05:06 +0200

    prova milestone 2.

--- main.js
+++ main.js
@@ -18,8 +18,10 @@ function callApi(){
       query : user,
     },
     success: function(data){
-
+      // stampo i risultati
       printResult(data);
+      //stampo le stelle che corrispondono ai voti
+      printStars(data);
 
     },
     errorr: function(){
@@ -28,7 +30,7 @@ function callApi(){
 
     },
   })
-}
+};
 
 function printResult(data){
 
@@ -40,11 +42,60 @@ function printResult(data){
     var title = answer[i].title;
     var original = answer[i].original_title;
     var language = answer[i].original_language;
-    var vote = answer[i].vote_average;
+    // var vote = answer[i].vote_average;
 
     $('ul#title').append('<li>'+ title +'</li>');
     $('ul#titolo-originale').append('<li>'+ original +'</li>');
     $('ul#lingua').append('<li>'+ language +'</li>');
-    $('ul#voto').append('<li>' + vote + '</li>');
+    // $('ul#voto').append('<li>' + vote + '</li>');
+
   }
-}
+};
+
+function printStars (data){
+  var star = data.results;
+
+  for(i = 0; i < star.length; i++){
+    var prova = star[i].vote_average;
+    var item = $('.item').clone();
+    var a = $(item).children();
+
+    // trasformo il voto in un numero da 1 a 5
+
+    if(prova === 0 && prova <= 2){
+      prova = 1;
+      // $('ul#voto').append('<i class="far fa-star"></i>');
+      $(a).css('far fa-star"');
+      console.log(a);
+      // console.log(prova);
+     }
+    //  else if((prova > 2) && (prova <= 4)){
+    //    prova = 2;
+    //   console.log(prova);
+    //   for(a = 0; a <= 1; a++){
+    //     $('ul#voto').append('<i class="far fa-star"></i>');
+    //   }
+    // }
+    // else if((prova > 4) && (prova <= 6)){
+    //   prova = 3;
+    //   console.log(prova)
+    //   for(b = 0; b <= 2; b++){
+    //     $('ul#voto').append('<i class="far fa-star"></i>');
+    //   }
+    // }
+    // else if((prova > 6) && (prova <= 8)){
+    //   prova = 4;
+    //   console.log(prova)
+    //   for(c = 0; c <= 3; c++){
+    //     $('ul#voto').append('<i class="far fa-star"></i>');
+    //   }
+    // }
+    // else if((prova > 8) && (prova <= 10)){
+    //   prova = 5;
+    //   console.log(prova)
+    //   for(d = 0; d <= 4; d++){
+    //     $('ul#voto').append('<i class="far fa-star"></i>');
+    //   }
+    // }
+  }
+};
